1ef34485056e9a2a2eb90f16e3efef2c6d954128
hionjk <161895508+hionjk@users.noreply.github.com>
2025-01-22 18:19:26 +0000

    Update snowflake.js

--- scripts/snowflake.js
+++ scripts/snowflake.js
@@ -3,7 +3,7 @@ const max_animation_time = 8;
 
 const particlesData = [
   {
-    path: 'https://github.com/hionjk/podarok_dlia_mileni/blob/main/images/snowflake-6.png',
+    path: 'https://github.com/hionjk/podarok_dlia_mileni/blob/main/images/snowflake-6.png?raw=true',
     minWidth: 24,
     maxWidth: 32,
   },
